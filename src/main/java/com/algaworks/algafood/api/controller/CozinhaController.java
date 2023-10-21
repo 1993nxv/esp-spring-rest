4524e706df0edee3dbb4711fbac01845c4299a10
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2023-10-21 17:22:16 -0300

    Implementando Content Negotiation

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -1,9 +1,15 @@
 package com.algaworks.algafood.api.controller;
 
+
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +17,25 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(path = "/cozinhas", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 public class CozinhaController {
 	
 	@Autowired
 	CozinhaRepository cozinhaRepository; 
 	
 	@GetMapping
-	public List<Cozinha> listarTodas(){
-		return cozinhaRepository.listar();
+	public List<Cozinha> findAll(){
+		return cozinhaRepository.findAll();
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Optional<Cozinha>> findById(@PathVariable Long id){
+		Optional<Cozinha> cozinha = cozinhaRepository.findById(id);
+		return ResponseEntity.ok().body(cozinha);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Long id){
+		return ResponseEntity.noContent().build();
 	}
 }
