0cd241d5a8d6ebde11a7d82c5027b613df2e38d7
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2023-11-02 21:53:55 -0300

    Corrigindo o Status HTTP para resource inexistente.

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -31,10 +30,17 @@ public class CozinhaController {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Optional<Cozinha>> findById(@PathVariable Long id){
+		
 		Optional<Cozinha> cozinha = cozinhaRepository.findById(id);
 
 //		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-		return ResponseEntity.ok(cozinha);
+	
+		if(!cozinha.isEmpty()) {
+			return ResponseEntity.ok(cozinha);
+		} else {
+			return ResponseEntity.noContent().build();	
+		}
+		
 	}
 	
 	@DeleteMapping("/{id}")
