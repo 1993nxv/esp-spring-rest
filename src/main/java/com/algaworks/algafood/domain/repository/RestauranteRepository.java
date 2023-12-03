c21d2e7e882c6d289bbddd222fc1ae4cbf5e4828
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2023-12-02 21:39:54 -0300

    Extraindo SDJ personalizado para interface, evitando problemas em tempo de exec.

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -7,13 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository extends 
+				JpaRepository<Restaurante, Long>,
+				RestauranteRepositoryQueries {
 	
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
 	//@Query("from Restaurante where nome like %:nome% and cozinha.id = :cozinhaId")
 	List<Restaurante> porNomeAndCozinhaId(String nome, Long cozinhaId);
 	
-	public List<Restaurante> findImpl(
-			String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
+//	public List<Restaurante> findImpl(
+//			String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
 }
