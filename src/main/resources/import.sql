e06a9c9d9c27495fe25c6ebb4cc5d35db3c43d53
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2023-12-11 11:20:06 -0300

    Mapeando relacionamento muitos-para-muitos com @ManyToMany. (Formas de Pagamento)

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -18,6 +18,9 @@ insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3);
 insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
 insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
 insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
+insert into forma_pagamento (id, descricao) values (4, 'Pix');
 
 insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (2, 4), (3, 1), (3, 4);
