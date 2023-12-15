065c2084e296586bd4f3f2c6ba37c09043dcea0c
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2023-12-14 22:45:35 -0300

    Desafios mapeando entidades, @ManyToOne, @OnoToMany e @ManyToMany.

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -14,6 +14,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -63,5 +64,9 @@ public class Restaurante {
 			   joinColumns = @JoinColumn(name ="restaurante_id"),
 			   inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "restaurante")
+	private List<Produto> produtos;
 	
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -23,3 +23,6 @@ insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Pe
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
 
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
+
+insert into produto (id, nome, descricao, preco, ativo, restaurante_id) values (1, 'Coca-cola', 'Bebida', 3.5, true, 1);
+insert into produto (id, nome, descricao, preco, ativo, restaurante_id) values (2, 'Prato Principal', 'Comida', 15.5, true, 1);
