2a571f8285d56326804c9f640915f7c8b4787fd4
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-01-05 20:15:47 -0300

    Desafio - Criando migrações e mapeando as entidades Pedido e ItemPedido.

--- src/main/java/com/algaworks/algafood/domain/model/Pedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -3,7 +3,15 @@ package com.algaworks.algafood.domain.model;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -13,24 +21,31 @@ import lombok.EqualsAndHashCode;
 @Entity
 public class Pedido {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private BigDecimal subTotal;
-	
 	private BigDecimal taxaFrete;
-	
 	private BigDecimal valorTotal;
 	
 	private Usuario cliente;
 	
 //	private List<ItemPedido> itemPedido;
 	
+	@Embedded
 	private Endereco enderecoEntrega;
 	
+	@ManyToOne
+	@JoinColumn(nullable = false)
 	private Restaurante restaurante;
 	
+	@ManyToOne
+	@JoinColumn(nullable = false)
 	private FormaPagamento formaPagamento;
 	
+	@CreationTimestamp
 	private LocalDateTime dataCriacao;
 	
 	private LocalDateTime dataConfirmacao;
