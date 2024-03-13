2c2b643ca7c413206cb4cc692c7548ad48e08667
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-03-12 22:10:35 -0300

    Desafio - usando @JsonIgnoreProperties e Jackson Mixin.

--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -2,7 +2,9 @@ package com.algaworks.algafood.core.jackson;
 
 import org.springframework.stereotype.Component;
 
+import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.model.mixin.CidadeMixin;
 import com.algaworks.algafood.domain.model.mixin.RestauranteMixin;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -13,5 +15,6 @@ public class JacksonMixinModule extends SimpleModule {
 	
 	public JacksonMixinModule () {
 		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+		setMixInAnnotation(Cidade.class, CidadeMixin.class);
 	}
 }
