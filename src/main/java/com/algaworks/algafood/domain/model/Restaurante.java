a535c3b359cea066d5908c7355a85d8236c7fa35
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-03-12 21:00:07 -0300

    Criando classes de mixin para usar as anotações do Jackson.

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -28,8 +28,6 @@ import com.algaworks.algafood.Groups;
 import com.algaworks.algafood.core.validation.Multiplo;
 import com.algaworks.algafood.core.validation.TaxaFrete;
 import com.algaworks.algafood.core.validation.ValorZeroIncluiDescricao;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -62,35 +60,29 @@ public class Restaurante {
 	private BigDecimal taxaFrete;
 	
 	@Valid
-	@JsonIgnoreProperties(value = "nome", allowGetters = true)
 	@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
 	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
 	
-	@JsonIgnore
 	@Embedded
 	private Endereco endereco;
 	
-	@JsonIgnore
 	@CreationTimestamp
 	@Column(nullable = false, columnDefinition = "datetime(2)")
 	private LocalDateTime dataCadastro;
 	
-	@JsonIgnore
 	@UpdateTimestamp
 	@Column(nullable = false, columnDefinition = "datetime(2)")
 	private LocalDateTime dataAtualizacao;
 	
-//	@JsonIgnore
 	@ManyToMany //(fetch = FetchType.EAGER)
 	@JoinTable(name = "restaurante_forma_pagamento", 
 			   joinColumns = @JoinColumn(name ="restaurante_id"),
 			   inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento;
 	
-	@JsonIgnore
 	@OneToMany(mappedBy = "restaurante")
 	private List<Produto> produtos;
 	
