cc6db1ba490bc45786fa438eb556f8bc355aa1bf
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-05-15 23:15:49 -0300

    Limitando os campos retornados pela API com Squiggly - Setando filtro apenas para /restaurantes/* e /pedidos/*.

--- src/main/java/com/algaworks/algafood/core/squiggly/SquigglyConfig.java
+++ src/main/java/com/algaworks/algafood/core/squiggly/SquigglyConfig.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.core.squiggly;
 
+import java.util.Arrays;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +19,13 @@ public class SquigglyConfig {
 		
 		Squiggly.init(mapper, new RequestSquigglyContextProvider());
 		
+		var urlPatterns = Arrays.asList("/pedidos/*", "/restaurantes/*");
+		
 		var filterRegistration = new FilterRegistrationBean<SquigglyRequestFilter>();
 		
 		filterRegistration.setFilter(new SquigglyRequestFilter());
 		filterRegistration.setOrder(1);
+		filterRegistration.setUrlPatterns(urlPatterns);
 		
 		return filterRegistration;
 	}
