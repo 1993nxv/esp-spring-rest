06f4d643756b289a0ee112a09ba265307926bf3b
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-06-12 21:25:28 -0300

    Implementando upload de arquivo com multipart-form-data - Curso Online Especialista Spring REST. Controller test

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -1,15 +1,39 @@
 package com.algaworks.algafood.api.controller;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
 public class RestauranteProdutoFotoController {
 	
-	@PutMapping
-	public void atualizarFoto() {
+	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public void atualizarFoto(
+			@PathVariable Long restauranteId,
+			@PathVariable Long produtoId,
+			@RequestParam MultipartFile arquivo) {
 		
+		var nomeArquivo = UUID.randomUUID().toString() + "_" + arquivo.getOriginalFilename();
+		
+		var arquivoFoto = Path.of("/Users/Delmondes/Desktop/catalogo", nomeArquivo);
+		
+		System.out.println(arquivoFoto);
+		System.out.println(arquivo.getContentType());
+		
+		try {
+			arquivo.transferTo(arquivoFoto);
+			System.out.println("Arquivo salvo com sucesso!");
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
