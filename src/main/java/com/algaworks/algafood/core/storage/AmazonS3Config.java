18fde314b9bdde20c78c84e736f107663c83019d
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-07-05 22:40:05 -0300

    Definindo bean do client da Amazon S3 e configurando credenciais.

---
+++ src/main/java/com/algaworks/algafood/core/storage/AmazonS3Config.java
@@ -0,0 +1,16 @@
+package com.algaworks.algafood.core.storage;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class AmazonS3Config {
+	
+	@Bean
+	public AmazonS3 amazonS3() {
+		return AmazonS3ClientBuilder.standard().build();
+	}
+}
--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -2,14 +2,20 @@ package com.algaworks.algafood.infrastructure.service.storage;
 
 import java.io.InputStream;
 import java.nio.file.Path;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.algaworks.algafood.domain.model.FotoProduto;
 import com.algaworks.algafood.domain.service.FotoStorageService;
+import com.amazonaws.services.s3.AmazonS3;
 
 @Service
 public class S3FotoStorageService implements FotoStorageService {
-
+	
+	@Autowired
+	private AmazonS3 amazonS3;
+	
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
 
