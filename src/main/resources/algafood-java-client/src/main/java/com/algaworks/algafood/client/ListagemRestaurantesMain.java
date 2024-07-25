2aa4a781cc4adec38dfd31ec85dfd427c2c6f4b0
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-07-24 23:16:11 -0300

    Tratando respostas com código de erro no client Java.

--- src/main/resources/algafood-java-client/src/main/java/com/algaworks/algafood/client/ListagemRestaurantesMain.java
+++ src/main/resources/algafood-java-client/src/main/java/com/algaworks/algafood/client/ListagemRestaurantesMain.java
@@ -2,18 +2,22 @@ package com.algaworks.algafood.client;
 
 import org.springframework.web.client.RestTemplate;
 
+import com.algaworks.algafood.client.api.ClientApiException;
 import com.algaworks.algafood.client.api.RestauranteClient;
 
 public class ListagemRestaurantesMain {
 
 	public static void main(String[] args) {
-		RestTemplate restTemplate = new RestTemplate();
+		try {
+			RestTemplate restTemplate = new RestTemplate();
 		
-		RestauranteClient restauranteClient = new RestauranteClient(
-				restTemplate, "http://api.algafoods.local:8080");
-		
-		restauranteClient.listar().stream()
-			.forEach(restaurante -> System.out.println(restaurante));
+			RestauranteClient restauranteClient = new RestauranteClient(
+					restTemplate, "http://api.algafoods.local:8080");
+			
+			restauranteClient.listar().stream()
+				.forEach(restaurante -> System.out.println(restaurante));
+		} catch (ClientApiException e) {
+			System.out.println(e.getProblem());
+		}
 	}
-	
 }
