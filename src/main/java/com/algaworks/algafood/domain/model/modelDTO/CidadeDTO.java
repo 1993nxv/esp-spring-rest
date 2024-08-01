34410e08961a0632129bdb82d5c61adb22d4a578
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-07-31 21:43:39 -0300

    Descrevendo modelos de representações e suas propriedades.

--- src/main/java/com/algaworks/algafood/domain/model/modelDTO/CidadeDTO.java
+++ src/main/java/com/algaworks/algafood/domain/model/modelDTO/CidadeDTO.java
@@ -1,14 +1,20 @@
 package com.algaworks.algafood.domain.model.modelDTO;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+@ApiModel(value = "Cidade", description = "Representa uma cidade")
 @Getter
 @Setter
 public class CidadeDTO {
 	
+	@ApiModelProperty(value = "ID da cidade", example = "1")
 	private Long id;
+	@ApiModelProperty(example = "São Paulo")
 	private String nome;
+
 	private EstadoDTO estado;
 
 }
--- src/main/java/com/algaworks/algafood/domain/model/modelVO/CidadeVO.java
+++ src/main/java/com/algaworks/algafood/domain/model/modelVO/CidadeVO.java
@@ -4,16 +4,21 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+@ApiModel(value = "Cidade", description = "Representa uma cidade")
 @Getter
 @Setter
 public class CidadeVO {
 	
+	@ApiModelProperty(example = "São Paulo")
 	@NotBlank
 	private String nome;
-		
+	
+	@ApiModelProperty(example = "1")
 	@Valid
 	@NotNull
 	private EstadoIdVO estado;
