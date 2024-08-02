63065de08b31f361ec065ffbe62f8f44617968bb
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-08-01 21:19:39 -0300

    Descrevendo códigos de status de respostas de forma global.

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,14 +1,21 @@
 package com.algaworks.algafood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.http.HttpStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -26,10 +33,25 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 //				.paths(PathSelectors.ant("/restaurantes/*"))
 				.build()
 			.apiInfo(apiInfo())
+			.useDefaultResponseMessages(false)
+			.globalResponseMessage(RequestMethod.GET, globalGetResponseMessages())
 			.tags(new Tag("Cidades", "Gerencia as cidades"));
 	}
 	
-	public ApiInfo apiInfo() {
+	private List<ResponseMessage> globalGetResponseMessages() {
+		return Arrays.asList(
+				new ResponseMessageBuilder()
+					.code(HttpStatus.SC_INTERNAL_SERVER_ERROR)
+					.message("Erro interno do servidor")
+					.build(),
+				new ResponseMessageBuilder()
+					.code(HttpStatus.SC_NOT_ACCEPTABLE)
+					.message("Recurso não possui representação aceita pelo consumidor")
+					.build()
+		);
+	}
+	
+	private  ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
 				.title("AlgaFood API")
 				.description("API aberta para clientes e restaurantes")
