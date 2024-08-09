c4ac89b2a87794202d1cee03a2ba2e9687cb410f
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-08-08 22:06:09 -0300

    Desacoplando anotações do Swagger dos controladores.

--- src/main/java/com/algaworks/algafood/api/controller/GrupoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/GrupoController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +18,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.assembler.GrupoDTOassembler;
+import com.algaworks.algafood.api.controller.openapi.GrupoControllerOpenApi;
 import com.algaworks.algafood.api.disassembler.GrupoVOdisassembler;
-import com.algaworks.algafood.api.exceptionhendler.Problem;
 import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Grupo;
@@ -28,18 +27,10 @@ import com.algaworks.algafood.domain.model.modelDTO.GrupoDTO;
 import com.algaworks.algafood.domain.model.modelVO.GrupoVO;
 import com.algaworks.algafood.domain.service.GrupoService;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 
-@Api(tags = "Grupos")
 @RestController
 @RequestMapping("/grupos")
-@Import(BeanValidatorPluginsConfiguration.class)
-public class GrupoController {
+public class GrupoController implements GrupoControllerOpenApi {
 
 	@Autowired
 	private GrupoService grupoService;
@@ -50,30 +41,19 @@ public class GrupoController {
 	@Autowired
 	private GrupoVOdisassembler grupoVOdisassembler;
 	
-	@ApiOperation("Lista os grupos")
 	@GetMapping
 	public List<GrupoDTO> findAll() {
 		return grupoDTOassembler.toListDTO(grupoService.findAll());
 	}
-	
-	@ApiOperation("Busca o grupo por id")
-	@ApiResponses({
-		@ApiResponse(code = 400, message = "Id do grupo inválido", response = Problem.class),
-		@ApiResponse(code = 404, message = "Grupo não encontrado", response = Problem.class)
-	})
+
 	@GetMapping("/{id}")
-	public GrupoDTO findById(
-			@ApiParam(value = "ID de um grupo", example = "1")
-			@PathVariable Long id) {
+	public GrupoDTO findById(@PathVariable Long id) {
 		return grupoDTOassembler.grupoDTOConverter(grupoService.findById(id));
 	}
 	
-	@ApiOperation("Cadastra um grupo")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public GrupoDTO save(
-			@ApiParam(name = "corpo", value = "Representação de um grupo")
-			@RequestBody @Valid GrupoVO grupoVO) {
+	public GrupoDTO save(@RequestBody @Valid GrupoVO grupoVO) {
 		try {
 			return grupoDTOassembler
 					.grupoDTOConverter(grupoService.save(grupoVOdisassembler.grupoVOConverter(grupoVO)));
@@ -82,13 +62,8 @@ public class GrupoController {
 		}
 	}
 	
-	@ApiOperation("Atualiza um grupo por Id")
 	@PutMapping("/{id}")
-	public GrupoDTO update(
-			@ApiParam(value = "ID de um grupo", example = "1")
-			@PathVariable Long id,
-			@ApiParam(name = "corpo", value = "Representação de um grupo")
-			@RequestBody @Valid GrupoVO grupoVO) {
+	public GrupoDTO update(@PathVariable Long id, @RequestBody @Valid GrupoVO grupoVO) {
 		Grupo grupoAtual = grupoService.findById(id);
 		grupoVOdisassembler.copyToDomainObj(grupoVO, grupoAtual);
 		try {
@@ -98,23 +73,15 @@ public class GrupoController {
 		}
 	}
 	
-	@ApiOperation("Atualiza um grupo parcialmente por Id")
 	@PatchMapping("/{id}")
-	public GrupoDTO updatePartially(
-			@ApiParam(value = "ID de um grupo", example = "1")
-			@PathVariable Long id, 
-			@ApiParam(name = "corpo", value = "Representação de um grupo")
-			@RequestBody GrupoVO grupoVO) {
+	public GrupoDTO updatePartially(@PathVariable Long id, @RequestBody GrupoVO grupoVO) {
 		Grupo grupo = grupoVOdisassembler.grupoVOConverter(grupoVO);
 		return grupoDTOassembler.grupoDTOConverter(grupoService.updatePartially(id, grupo));
 	}
 	
-	@ApiOperation("Exclui um grupo por Id")
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void deleteById(
-			@ApiParam(value = "ID de um grupo", example = "1")
-			@PathVariable Long id) {
+	public void deleteById(@PathVariable Long id) {
 		grupoService.deleteById(id);
 	}
 
--- src/main/java/com/algaworks/algafood/api/controller/openapi/GrupoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/controller/openapi/GrupoControllerOpenApi.java
@@ -1,5 +1,62 @@
 package com.algaworks.algafood.api.controller.openapi;
 
-public interface GrupoControllerOpenApi {
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.algaworks.algafood.api.exceptionhendler.Problem;
+import com.algaworks.algafood.domain.model.modelDTO.GrupoDTO;
+import com.algaworks.algafood.domain.model.modelVO.GrupoVO;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+
+@Api(tags = "Grupos")
+@Import(BeanValidatorPluginsConfiguration.class)
+public interface GrupoControllerOpenApi {
+	
+	@ApiOperation("Lista os grupos")
+	public List<GrupoDTO> findAll();
+	
+	@ApiOperation("Busca o grupo por id")
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "Id do grupo inválido", response = Problem.class),
+		@ApiResponse(code = 404, message = "Grupo não encontrado", response = Problem.class)
+	})
+	public GrupoDTO findById(
+			@ApiParam(value = "ID de um grupo", example = "1")
+			@PathVariable Long id);
+	
+	@ApiOperation("Cadastra um grupo")
+	public GrupoDTO save(
+			@ApiParam(name = "corpo", value = "Representação de um grupo")
+			@RequestBody @Valid GrupoVO grupoVO);
+	
+	@ApiOperation("Atualiza um grupo por Id")
+	public GrupoDTO update(
+			@ApiParam(value = "ID de um grupo", example = "1")
+			@PathVariable Long id,
+			@ApiParam(name = "corpo", value = "Representação de um grupo")
+			@RequestBody @Valid GrupoVO grupoVO);
+	
+	@ApiOperation("Atualiza um grupo parcialmente por Id")
+	public GrupoDTO updatePartially(
+			@ApiParam(value = "ID de um grupo", example = "1")
+			@PathVariable Long id, 
+			@ApiParam(name = "corpo", value = "Representação de um grupo")
+			@RequestBody GrupoVO grupoVO);
+	
+	@ApiOperation("Exclui um grupo por Id")
+	public void deleteById(
+			@ApiParam(value = "ID de um grupo", example = "1")
+			@PathVariable Long id);
+	
 }
