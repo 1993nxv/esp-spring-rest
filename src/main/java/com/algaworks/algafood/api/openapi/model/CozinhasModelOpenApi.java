9d4def7da64d38e3713b7c6b0918df37384b64a7
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-08-12 21:44:47 -0300

    Corrigindo documentação com substituição Page cozinhas.

--- src/main/java/com/algaworks/algafood/api/openapi/model/CozinhasModelOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/model/CozinhasModelOpenApi.java
@@ -1,32 +1,10 @@
 package com.algaworks.algafood.api.openapi.model;
 
-import java.util.List;
-
 import com.algaworks.algafood.domain.model.modelDTO.CozinhaDTO;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @ApiModel("CozinhasModel")
-public class CozinhasModelOpenApi {
-	
-	
-	private List<CozinhaDTO> content;
-	
-	@ApiModelProperty(example = "10", value = "Quantidade de regitros por página")
-	private Long size;
-	
-	@ApiModelProperty(example = "50", value = "Total de registros")
-	private Long totalElements;
-	
-	@ApiModelProperty(example = "5", value = "Total de páginas")
-	private Long totalPages;
-	
-	@ApiModelProperty(example = "0", value = "Número da página, começa em 0")
-	private Long number;
+public class CozinhasModelOpenApi extends PagedModelOpenApi<CozinhaDTO>{
 	
 }
