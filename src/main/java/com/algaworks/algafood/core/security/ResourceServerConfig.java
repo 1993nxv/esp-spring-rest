4c99c6a2741cc1468787a503aee948f3d3a4903f
Delmondes <wellington.apk@gmail.com>
2024-09-12 21:43:09 -0300

    Conhecendo o fluxo para emitir e usar Refresh Tokens.

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -13,9 +13,9 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-				.authorizeRequests()
+			.authorizeRequests()
 				.anyRequest().authenticated()
-				.and()
+			.and()
 				.oauth2ResourceServer()
 				.opaqueToken();
 	}
