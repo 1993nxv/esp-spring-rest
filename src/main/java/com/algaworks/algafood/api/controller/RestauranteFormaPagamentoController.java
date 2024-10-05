11f77f4ac57d6bd1e9e33579206bdde02d87d20e
Delmondes <wellington.apk@gmail.com>
2024-10-04 22:06:18 -0300

    Restringindo acessos de forma contextual (sensível à informação).

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
@@ -1,23 +1,16 @@
 package com.algaworks.algafood.api.controller;
 
-import java.util.List;
-
-import com.algaworks.algafood.core.security.CheckSecurity;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.algaworks.algafood.api.assembler.FormaPagementoDTOassembler;
+import com.algaworks.algafood.core.security.CheckSecurity;
 import com.algaworks.algafood.domain.model.FormaPagamento;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.model.modelDTO.FormaPagamentoDTO;
 import com.algaworks.algafood.domain.service.RestauranteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/formas-pagamento")
@@ -36,18 +29,19 @@ public class RestauranteFormaPagamentoController {
 		return DTOassembler.toListDTO(restaurante.getFormasPagamento());
 	}
 
-	@CheckSecurity.Restaurantes.PodeGerenciarCadastro
-	@DeleteMapping("/{formaPagamentoId}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void removeFormaPagamento(@PathVariable Long restauranteId, @PathVariable Long formaPagamentoId) {
-		restauranteService.removeFormaPagamento(restauranteId, formaPagamentoId);
-	}
-
-	@CheckSecurity.Restaurantes.PodeGerenciarCadastro
+	@CheckSecurity.Restaurantes.PodeGerenciarFuncionamento
 	@PostMapping("/{formaPagamentoId}")
 	@ResponseStatus(HttpStatus.OK)
 	public List<FormaPagamentoDTO> adicionaFormaPagamento(@PathVariable Long restauranteId, @PathVariable Long formaPagamentoId) {
 		List<FormaPagamento> formaPagamentos = restauranteService.adicionaFormaPagamento(restauranteId, formaPagamentoId);
 		return DTOassembler.toListDTO(formaPagamentos);
 	}
+
+	@CheckSecurity.Restaurantes.PodeGerenciarFuncionamento
+	@DeleteMapping("/{formaPagamentoId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void removeFormaPagamento(@PathVariable Long restauranteId, @PathVariable Long formaPagamentoId) {
+		restauranteService.removeFormaPagamento(restauranteId, formaPagamentoId);
+	}
+
 }
