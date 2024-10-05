f2a814a9fb134b72c9124377a393416a3b971faa
Delmondes <wellington.apk@gmail.com>
2024-10-04 22:08:03 -0300

    Restringindo acessos de forma contextual (sensível à informação).

--- src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
+++ src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
@@ -29,10 +29,7 @@ public @interface CheckSecurity {
         @Retention(RetentionPolicy.RUNTIME)
         @Target(ElementType.METHOD)
         public @interface PodeGerenciarCadastro { }
-
-//        @PreAuthorize("hasAuthority('SCOPE_WRITE') " +
-//                "and hasAuthority('EDITAR_RESTAURANTES') or " +
-//                "@algaSecurity.gerenciaRestaurante(#restauranteId)")
+        
         @PreAuthorize("hasAuthority('SCOPE_WRITE') and " +
                 "hasAuthority('EDITAR_RESTAURANTES') or " +
                 "(@algaSecurity.gerenciaRestaurante(#restaurenteId))")
