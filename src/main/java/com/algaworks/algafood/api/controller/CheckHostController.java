ead03fcaeb563a33cd711182e3e14b02ef4192a9
Delmondes <wellington.apk@gmail.com>
2024-12-04 23:48:33 -0300

    Controller hostcheck

---
+++ src/main/java/com/algaworks/algafood/api/controller/CheckHostController.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.api.ResourceUriHelper;
+import com.algaworks.algafood.api.assembler.CidadeDTOassembler;
+import com.algaworks.algafood.api.disassembler.CidadeVOdisassembler;
+import com.algaworks.algafood.api.openapi.controller.CidadeControllerOpenApi;
+import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
+import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.modelDTO.CidadeDTO;
+import com.algaworks.algafood.domain.model.modelVO.CidadeVO;
+import com.algaworks.algafood.domain.service.CidadeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class CheckHostController {
+
+	@GetMapping("/hostcheck")
+	public String checkHost() throws UnknownHostException {
+		return InetAddress.getLocalHost().getHostAddress()
+				+ " - " + InetAddress.getLocalHost().getHostName();
+	}
+}
